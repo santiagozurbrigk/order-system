9c0d3c759f439d9bbab6d1447cd33b10b6062b0a
matreck5 <matiiaschao2@gmail.com>
2025-09-05 00:08:34 -0300

    commit 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = ({ cartItems, onCartClick }) => {
               <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2.5 5M7 13l2.5 5m6-5v6a2 2 0 01-2 2H9a2 2 0 01-2-2v-6m8 0V9a2 2 0 00-2-2H9a2 2 0 00-2 2v4.01" />
               </svg>
-              <span>Carrito</span>
+              
               {totalItems > 0 && (
                 <span className="absolute -top-1 -right-1 bg-slate-600 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
                   {totalItems}
